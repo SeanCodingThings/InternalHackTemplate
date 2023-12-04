23066ba9ebc938c63cadf69b62081f041035e462
Vaxaop <vaxaop@gmail.com>
2023-12-04 12:57:45 -0500

    Adding comments to the main.c file.

--- InternalHackTemplate/src/main.c
+++ InternalHackTemplate/src/main.c
@@ -9,16 +9,19 @@ void HackFunction(HINSTANCE instance)
     printf("Console was allocated succesfully!\n");
     while (!GetAsyncKeyState(VK_ESCAPE))
     {
-        Sleep(100);
+        Sleep(100); // I do not wan't to put to much stress on the CPU!
     }
     FreeConsole();
+    // FreeLibray unloads the .dll of the program, ExitThread closes the current thread. (They are two functions combined into one!)
     FreeLibraryAndExitThread(instance, EXIT_SUCCESS);
 }
 
+// The reason we are using __stdcall, is because this is the function call that the windows API uses.
 bool __stdcall DllMain(HINSTANCE instance, unsigned long reason, void* reserved)
 {
     if ( reason == DLL_PROCESS_ATTACH )
     {
+        // We are creating an extra thread to be able to write code with out the whole program stopping.
         void* thread = CreateThread
         (
             NULL,
@@ -28,10 +31,12 @@ bool __stdcall DllMain(HINSTANCE instance, unsigned long reason, void* reserved)
             0,
              NULL
         );
+        // Checking if Creating the thread failed, and if it does, we close the thread.
         if (thread == NULL)
         {
             CloseHandle(thread);
         }
     }
+    // Returning true on success. (PS: that is how the DllMain function works.)
     return true;
 }
